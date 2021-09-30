002541753c013fb2953f2177d12ae89903fda39a
Julian362 <54406277+Julian362@users.noreply.github.com>
2021-09-30 13:46:08 -0500

    Practica Sesion 8

--- ComponentePractico8/app.py
+++ ComponentePractico8/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
-app = Flask(__name__,template_folder='static/templates')
+app = Flask(__name__)
 @app.route('/')
 
-@app.route('/registro')
 def registro():
     return render_template('formulario.html')
